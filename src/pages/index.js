16249514c32e28c117c27f0c088661980e1103cb
Claire V. Hammond <me@cvhammond.com>
2024-06-14 21:23:35 -0700

    fix mission link

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg hero__button"
-            to="/docs/intro">
+            to="/mission">
             Our Mission
           </Link>
         </div>
